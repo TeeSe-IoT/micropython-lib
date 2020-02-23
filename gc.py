b6c5dd9bda35c58c128cafb230cfeb27e454d9d5
Sternenkreuz <jan.schrader@live.de>
2020-02-23 17:31:14 +0100

    update

---
+++ gc.py
@@ -0,0 +1,26 @@
+"""
+Module: 'gc' on esp32 1.11.0
+"""
+# MCU: (sysname='esp32', nodename='esp32', release='1.11.0', version='v1.11-132-gc24d81119 on 2019-07-08', machine='ESP32 module with ESP32')
+# Stubber: 1.2.0
+def collect():
+    pass
+
+def disable():
+    pass
+
+def enable():
+    pass
+
+def isenabled():
+    pass
+
+def mem_alloc():
+    pass
+
+def mem_free():
+    pass
+
+def threshold():
+    pass
+
